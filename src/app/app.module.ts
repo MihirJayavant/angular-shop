9bf300d6aa8fb29388203d1daf34a9ff8647864b
mihir jayavant <mihir.jayavant2@gmail.com>
2018-08-19 12:57:14 +0530

    dashboard module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { HomeComponent, LoginComponent } from './home'
+import { Routes, RouterModule } from '@angular/router'
+import { environment } from '../environments/environment'
+import { DashboardModule } from './dashboard/dashboard.module'
+
+const routes: Routes = [
+                        {path: 'home', component: HomeComponent},
+                        {path: '', pathMatch: 'full', redirectTo: 'home'}
+                      ]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, environment.production ? {} : {enableTracing: true}),
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
