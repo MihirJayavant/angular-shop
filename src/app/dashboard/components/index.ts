9403ebb57f25daaecd2ca246c3141815cdce7d6b
mihir jayavant <mihir.jayavant2@gmail.com>
2018-12-25 22:34:07 +0530

    added components for changes in UI

--- src/app/dashboard/components/index.ts
+++ src/app/dashboard/components/index.ts
@@ -3,6 +3,8 @@ import { CustomerFormsPageComponent } from './customer-forms-page/customer-forms
 import { CustomerDisplayPageComponent } from './customer-display-page/customer-display-page.component'
 import { CustomerCardComponent } from './customer-display-page/customer-card/customer-card.component'
 import { CustomerCardsComponent } from './customer-display-page/customer-cards/customer-cards.component'
+import { CustomerListComponent } from './customer-display-page/customer-list/customer-list.component'
+import { CustomerDetailsComponent } from './customer-display-page/customer-details/customer-details.component'
 
 export * from './dashboard.component'
 export * from './customer-forms-page/customer-forms-page.component'
@@ -13,5 +15,7 @@ export const components = [
   CustomerFormsPageComponent,
   CustomerDisplayPageComponent,
   CustomerCardComponent,
-  CustomerCardsComponent
+  CustomerCardsComponent,
+  CustomerListComponent,
+  CustomerDetailsComponent
 ]
