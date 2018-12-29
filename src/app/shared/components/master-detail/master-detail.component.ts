b49823e36abf2611d6b14a487a74e9f551f45164
mihir jayavant <mihir.jayavant2@gmail.com>
2018-12-29 20:15:06 +0530

    some progress

---
+++ src/app/shared/components/master-detail/master-detail.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'shared-master-detail',
+  templateUrl: './master-detail.component.html'
+})
+export class MasterDetailComponent {
+  constructor() {}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { NavbarComponent } from './components'
+import {
+  NavbarComponent,
+  MasterDetailComponent,
+  MasterComponent,
+  DetailComponent
+} from './components'
 import { ActiveDirective } from './directives'
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [NavbarComponent, ActiveDirective],
-  exports: [NavbarComponent]
+  declarations: [
+    NavbarComponent,
+    ActiveDirective,
+    MasterDetailComponent,
+    MasterComponent,
+    DetailComponent
+  ],
+  exports: [NavbarComponent, MasterDetailComponent, MasterComponent, DetailComponent]
 })
 export class SharedModule {}
