c62d8ae827c52f9ec707443d4e7104d617636a8a
Mihir Jayavant <mihir.jayavant2@gmail.com>
2020-06-27 15:23:21 +0530

    immutable removed

--- src/app/dashboard/pipes/filter-customer-type.pipe.ts
+++ src/app/dashboard/pipes/filter-customer-type.pipe.ts
@@ -1,23 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core'
 import { Customer, CustomerType } from '../models'
-import { List } from 'immutable'
 
 @Pipe({
-  name: 'filterCustomerType'
+  name: 'filterCustomerType',
 })
 export class FilterCustomerTypePipe implements PipeTransform {
-  public transform(value: List<Customer> | null, type: CustomerType | string): List<Customer> {
+  public transform(value: Customer[] | null, type: CustomerType | string): Customer[] {
     if (value === null) {
-      return List()
+      return []
     }
 
     switch (type) {
       case 'All':
         return value
       case CustomerType.basic:
-        return value.filter(p => (!!p ? p.type === CustomerType.basic : false)).toList()
+        return value.filter(p => (!!p ? p.type === CustomerType.basic : false))
       case CustomerType.lead:
-        return value.filter(p => (!!p ? p.type === CustomerType.lead : false)).toList()
+        return value.filter(p => (!!p ? p.type === CustomerType.lead : false))
       default:
         return value
     }
