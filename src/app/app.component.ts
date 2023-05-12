02af6ba7bc464ed100685ed9faedb1f1fbd41ced
Mihir Jayavant <mihir.jayawant@amwater.com>
2023-05-12 16:22:04 +0530

    standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styles: []
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styles: [],
+    standalone: true,
+    imports: [RouterOutlet]
 })
 export class AppComponent {}
