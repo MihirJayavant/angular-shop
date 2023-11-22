7fbe73839bbee010bd2f695d2a62e14337864e8f
Mihir Jayavant <mihir.jayavant2@gmail.com>
2023-11-22 20:21:28 +0530

    fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,19 @@
-import { HomePageComponent } from './pages/home/home-page.component'
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component'
 import { Routes } from '@angular/router'
 
 export const routes: Routes = [
-  { component: HomePageComponent, path: '', pathMatch: 'full' },
+  {
+    loadComponent: () =>
+      import('./pages/home/home-page.component').then(mod => mod.HomePageComponent),
+    path: '',
+    pathMatch: 'full',
+  },
   {
     loadChildren: () => import('./pages/dashboard/dashboard.routes').then(mod => mod.routes),
     path: 'dashboard',
+  },
+  {
+    component: PageNotFoundComponent,
+    path: '**',
   },
 ]
